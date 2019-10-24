15838a910e1c075024efdccb087c315a7df5a100
Lily <nathdubas@gmail.com>
2019-10-24 12:15:18 +0200

    ajout de while

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,2 +1,5 @@
-const prenom = prompt("Entrez votre prénom :");
-alert(`Bonjour, ${prenom}`);
+var firstName = window.prompt('Entrez votre prénom :');
+while(firstName == ''){
+  firstName=window.prompt('Veuillez saisir votre prénom');
+}
+window.alert(`Bonjour, ${firstName}`);
